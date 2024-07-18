2f26176d581808563354e0803891d8384534c17a
Chiro Hiro <chiro8x@gmail.com>
2024-07-18 18:53:37 +0700

    Update p2p chat

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,37 @@ import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
 import { tcp } from "@libp2p/tcp";
 import { multiaddr } from "@multiformats/multiaddr";
 import chalk from "chalk";
+import { unmarshalPrivateKey } from "@libp2p/crypto/keys";
+import {
+  createEd25519PeerId,
+  createFromProtobuf,
+  exportToProtobuf,
+} from "@libp2p/peer-id-factory";
+import { writeFileSync, readFileSync, existsSync } from "node:fs";
 import { createLibp2p } from "libp2p";
 import { createInterface } from "readline/promises";
 
 const PUBLIC_KAD_DHT = "/ipfs/kad/1.0.0";
-const TOPIC = "chat";
+const KEY_FILE = `${process.cwd()}/nodekey.bin`;
+const TOPIC = "orochi-network-p2p-chat";
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function createNode() {
+  if (!existsSync(KEY_FILE)) {
+    const peerId = await createEd25519PeerId();
+    console.log("Create peerId:", peerId.toString());
+    writeFileSync(KEY_FILE, exportToProtobuf(peerId));
+  }
+
+  const filecontent = readFileSync(KEY_FILE);
+  const peerId = await createFromProtobuf(filecontent);
+  const privKey = await unmarshalPrivateKey(peerId.privateKey!);
   return await createLibp2p({
+    peerId: peerId,
+    privateKey: privKey,
     addresses: {
       listen: ["/ip4/0.0.0.0/tcp/0"],
     },
@@ -45,30 +64,53 @@ async function createNode() {
 const node = await createNode();
 
 console.log(
-  "Service online:",
-  node.getMultiaddrs().map((e) => e.toString())
+  `Service online:\n${chalk.greenBright(
+    node
+      .getMultiaddrs()
+      .map((e) => e.toString())
+      .join("\n")
+  )}`
 );
 
-node.addEventListener("peer:connect", (evt: any) => {
-  const peerId = evt.detail;
-  console.log("Connection established to:", peerId.toString()); // Emitted when a peer has been found
-});
+const nodes: string[] = [];
+
+for (let i = 0; i < nodes.length; i += 1) {
+  try {
+    await node.dial(multiaddr(nodes[i]));
+  } catch (e: any) {
+    console.log(e.message);
+  }
+}
 
 if (process.argv.length === 3) {
   node.dial(multiaddr(process.argv[2]));
 }
 
-node.services.pubsub.addEventListener("message", (message) => {
+const prompt = () =>
+  `${chalk.bgBlue.white(new Date().toISOString())}${chalk.bgGreen.white(
+    "me"
+  )}${chalk.bgRed.white(":")}`;
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+node.services.pubsub.addEventListener("message", async (message) => {
   if (message.detail.topic === TOPIC) {
     const res = JSON.parse(new TextDecoder().decode(message.detail.data));
+    process.stdout.cursorTo(0);
+    process.stdout.clearLine(1);
     console.log(
       `${chalk.bgBlue.white(res.time)}${chalk.bgGreen.white(
-        res.from
-      )}${chalk.bgRed.white(res.message)}`
+        res.alias || res.from
+      )}${chalk.bgRed.white(":")}${res.message}`
     );
+    process.stdout.write(prompt());
   }
 });
 
+const name = await rl.question("What is your alias? ");
 node.services.pubsub.subscribe(TOPIC);
 
 let subscribers = 0;
@@ -76,20 +118,19 @@ while (subscribers === 0) {
   subscribers = node.services.pubsub.getSubscribers(TOPIC).length;
   await sleep(1000);
 }
-console.log("You're online");
-const rl = createInterface({
-  input: process.stdin,
-});
+
+console.log(chalk.blueBright(`Hi ${name}, you're online.\n`));
 
 while (true) {
-  const answer = await rl.question(">");
-  node.services.pubsub.publish(
+  const answer = await rl.question(prompt());
+  await node.services.pubsub.publish(
     TOPIC,
     new TextEncoder().encode(
       JSON.stringify({
         from: node.peerId.toString(),
         time: new Date().toISOString(),
         message: answer,
+        alias: name,
       })
     )
   );
